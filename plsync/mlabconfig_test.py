3fe3437603676a14104d75e203113c51ca88093a
Stephen Soltesz <soltesz@google.com>
2015-11-03 16:14:47 -0500

    Add "zone" as a supported format for mlabconfig.
    This change adds a new output format for mlabconfig to generate a zone
    file for measurement-lab.org. This form of generation should obsolete
    gen-zone.py.
    
    The mlabzone.header.in is taken from the current gen-zone.py script. The
    included version of mlabzone.header.in increases the zone expiration
    time from 1 day to 7 days. This should allow for longer periods of
    outages of the shadow master server.

--- plsync/mlabconfig_test.py
+++ plsync/mlabconfig_test.py
@@ -1,10 +1,12 @@
 """Tests for mlabconfig."""
 
-import mlabconfig
-import StringIO
-import unittest
-from planetlab import model
 import json
+import mlabconfig
+import optparse
+from planetlab import model
+import StringIO
+import time
+import unittest
 
 
 class MlabconfigTest(unittest.TestCase):
@@ -57,6 +59,96 @@ class MlabconfigTest(unittest.TestCase):
 
         results = json.loads(output.getvalue())
         self.assertItemsEqual(results, expected_results)
+
+    def test_export_router_and_switch_records(self):
+        output = StringIO.StringIO()
+        expected_results = [
+            mlabconfig.format_a_record('r1.abc01', '192.168.1.1'),
+            mlabconfig.format_a_record('s1.abc01', '192.168.1.2'),
+        ]
+
+        mlabconfig.export_router_and_switch_records(output, self.sites)
+
+        results = output.getvalue().split('\n')
+        self.assertContainsItems(results, expected_results)
+
+    def test_export_pcu_records(self):
+        output = StringIO.StringIO()
+        expected_results = [
+            mlabconfig.format_a_record('mlab1d.abc01', '192.168.1.4'),
+            mlabconfig.format_a_record('mlab2d.abc01', '192.168.1.5'),
+            mlabconfig.format_a_record('mlab3d.abc01', '192.168.1.6'),
+        ]
+
+        mlabconfig.export_pcu_records(output, self.sites)
+
+        results = output.getvalue().split('\n')
+        self.assertContainsItems(results, expected_results)
+
+    def test_export_server_records(self):
+        output = StringIO.StringIO()
+        # This is a subset of expected results.
+        expected_results = [
+            mlabconfig.format_a_record('mlab1.abc01', '192.168.1.9'),
+            mlabconfig.format_a_record('mlab2v4.abc01', '192.168.1.22'),
+            mlabconfig.format_aaaa_record('mlab3.abc01', '2400:1002:4008::35'),
+            mlabconfig.format_aaaa_record('mlab1v6.abc01', '2400:1002:4008::9')
+        ]
+
+        mlabconfig.export_server_records(output, self.sites)
+
+        results = output.getvalue().split('\n')
+        self.assertContainsItems(results, expected_results)
+
+    def test_export_experiment_records(self):
+        output = StringIO.StringIO()
+        experiments = [model.Slice(name='abc_bar', index=1, attrs=self.attrs,
+                                   users=self.users, use_initscript=True,
+                                   ipv6='all')]
+        expected_results = [
+            mlabconfig.format_a_record(
+                'bar.abc.abc01', '192.168.1.11'),
+            mlabconfig.format_a_record(
+                'bar.abc.mlab2.abc01', '192.168.1.24'),
+            mlabconfig.format_a_record(
+                'bar.abcv4.abc01', '192.168.1.11'),
+            mlabconfig.format_a_record(
+                'bar.abc.mlab2v4.abc01', '192.168.1.24'),
+            mlabconfig.format_aaaa_record(
+                'bar.abc.abc01', '2400:1002:4008::11'),
+            mlabconfig.format_aaaa_record(
+                'bar.abc.abc01', '2400:1002:4008::37'),
+            mlabconfig.format_aaaa_record(
+                'bar.abc.mlab3.abc01', '2400:1002:4008::37'),
+            mlabconfig.format_aaaa_record(
+                'bar.abcv6.abc01', '2400:1002:4008::11'),
+            mlabconfig.format_aaaa_record(
+                'bar.abc.mlab1v6.abc01', '2400:1002:4008::11'),
+        ]
+
+        mlabconfig.export_experiment_records(output, self.sites, experiments)
+
+        results = output.getvalue().split('\n')
+        self.assertContainsItems(results, expected_results)
+
+    def test_serial_rfc1912(self):
+        # Fri Oct 31 00:45:00 2015 UTC.
+        # 45-minutes should result in 03.
+        ts = 1446252300
+
+        serial = mlabconfig.serial_rfc1912(time.gmtime(ts))
+
+        self.assertEqual('2015103103', serial)
+
+    def test_export_mlab_zone_header(self):
+        options = optparse.Values()
+        options.value = 'middle'
+        output = StringIO.StringIO()
+        header = StringIO.StringIO('before; %(value)s; after')
+
+        mlabconfig.export_mlab_zone_header(output, header, options)
+
+        self.assertEqual(output.getvalue(), 'before; middle; after')
 
 
 if __name__ == '__main__':
